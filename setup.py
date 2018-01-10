6835796e91e46b49b76436085c25d27f3700c5ac
radar-bear <liangzibear@163.com>
2018-01-10 15:25:17 +0800

    fix-setup

--- setup.py
+++ setup.py
@@ -4,16 +4,13 @@ from subprocess import call
 
 platform = sys.paltform
 
-conda_package_list = ['plotly']
-pip_package_list = ['spacepy'] # some packages missed in conda
+pip_package_list = ['numpy', 'pandas', 'h5py', 'spacepy', 'plotly']
 
 package_dir = os.path.join(os.path.dirname(os.__file__),
                            'site-packages', 'spacepku')
 print(package_dir)
 
 call(['pip', 'install'] + pip_package_list)
-
-call(['conda', 'install'] + conda_package_list)
 
 if not os.path.exists(package_dir):
     os.mkdir(package_dir)
