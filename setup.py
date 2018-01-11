dd006dca1139a89544b47769375d582e53a4bb96
radar-bear <liangzibear@163.com>
2018-01-11 19:28:33 +0800

    fix-installation

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ import os
 import sys
 from subprocess import call
 
-# platform = sys.platform
+platform = sys.platform
+
 call('pip install -r requirements.txt', shell=True)
 
 import numpy
@@ -18,5 +19,22 @@ else:
 call(['cp -r ./src/* ' + package_dir], shell=True)
 
 call(['git clone https://github.com/rstoneback/pysatCDF.git && cd pysatCDF && python setup.py install'], shell=True)
+
+# dir_list = os.listdir('./pysatCDF/build')
+# for dir in dir_list:
+#     if dir[:3] == 'src':
+#         break
+
+call(['cp ./pysatCDF/build/src*/lib/libcdf* /usr/local/lib'], shell=True)
+
+if platform=='linux':
+    call(['echo "CDF_LIB = \\\"/usr/local/lib\\\"" >> /etc/environment'], shell=True)
+elif platform=='darwin':
+    call(['echo "export CDF_LIB=/usr/local/lib" >> /etc/bashrc'], shell=True)
+    # call(['echo "export CDF_LIB=/usr/local/lib" >> ~/.bashrc'], shell=True)
+    # call(['echo "export CDF_LIB=/usr/local/lib" >> ~/.profile'], shell=True)
+
+else:
+    raise ValueError('operation system not supported')
 
 call(['python', 'test.py'])
--- test.py
+++ test.py
@@ -1,3 +1,6 @@
+import os
+os.environ['CDF_LIB'] = '/usr/local/lib'
+
 import spacepku as sp
 from spacepku import data_obj
 from spacepku import plot_lines
