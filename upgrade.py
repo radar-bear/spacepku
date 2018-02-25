077354e3a4aae33b3323e76f7a9b65c2c6115969
radar-bear <liangzibear@163.com>
2018-02-25 12:07:36 +0800

    upgrade script

---
+++ upgrade.py
@@ -0,0 +1,21 @@
+import os
+import sys
+from subprocess import call
+
+platform = sys.platform
+
+call('pip install -r requirements.txt', shell=True)
+
+import numpy
+package_dir = (numpy.__file__).split('numpy')[0] + 'spacepku'
+print(package_dir)
+
+if not os.path.exists(package_dir):
+    os.mkdir(package_dir)
+else:
+    call(['rm -r ' + package_dir], shell=True)
+    os.mkdir(package_dir)
+
+call(['cp -r ./src/* ' + package_dir], shell=True)
+
+call(['python', 'test.py'])
