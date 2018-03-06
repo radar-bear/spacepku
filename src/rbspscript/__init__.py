c3b854aea1bed2de69041e1c2abee5003ef6ff1f
radar-bear <liangzibear@163.com>
2018-03-06 16:25:41 +0800

    add get wave to __init__

--- src/rbspscript/__init__.py
+++ src/rbspscript/__init__.py
@@ -1,1 +1,2 @@
 from .plot_rbsp_overview import *
+from .wave_angle_polar import get_wave_info
--- src/rbspscript/wave_angle_polar.py
+++ src/rbspscript/wave_angle_polar.py
@@ -1,6 +1,7 @@
 import spacepku as sp
 import spacepku.rbsptool as rbsp
 import pandas as pd
+import numpy as np
 import os
 import re
 
